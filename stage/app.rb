c666f4d3b5befb52b69953f1ef9c1db3355f5179
Benny Wong <bdotdub@gmail.com>
2008-12-26 01:36:58 -0500

    added sass handling

--- stage/app.rb
+++ stage/app.rb
@@ -15,6 +15,19 @@ def register_app
       raise Sinatra::NotFound
     end
   end
+  
+  get '/stylesheets/:sheet.css' do
+    @sheet = params[:sheet]
+
+    if File.exists?(Sinatra.application.options.views + "/#{@sheet}.sass")
+      # Render the page
+      content_type 'text/css', :charset => 'utf-8'
+      sass @sheet.to_sym
+    else
+      # Not found!
+      raise Sinatra::NotFound
+    end
+  end
 
 
 end
